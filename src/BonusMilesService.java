b3130f8866475a91ab89729bccf03533c04abf1d
Olia Brizgina <olvitbriz@gmail.com>
2023-01-20 13:59:52 +0400

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate (int price) {
+        int countBonusMiles;
+        countBonusMiles = price / 20;
+        return countBonusMiles;
+    }
+}
+
